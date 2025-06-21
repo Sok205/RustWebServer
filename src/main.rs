fbac67a6d97926f7224306883fbfec24ca65b13e
Filip Sokołowski <123462364+Sok205@users.noreply.github.com>
2025-06-21 15:57:38 +0200

    Server working

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,53 @@
+use RustWebServer::ThreadPool;
 use std::{
     fs,
     io::{BufReader, prelude::*},
     net::{TcpListener, TcpStream},
+    sync::atomic::{AtomicUsize, Ordering},
+    sync::Arc,
+    thread,
+    time::Duration,
 };
 
 fn main() {
-    // Bind to a TCP listener on port 7878
-    let listener = TcpListener::bind("127.0.0.1:7878").expect("Could not bind to address");
-    
-    // Listen for incoming connections
-    for stream in listener.incoming(){
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
+
+    let active_connections = Arc::new(AtomicUsize::new(0));
+    let connection_counter = Arc::new(AtomicUsize::new(1));
+
+    for stream in listener.incoming() {
         let stream = stream.unwrap();
-        
-        handle_connection(stream);
+        let active = Arc::clone(&active_connections);
+        let counter = Arc::clone(&connection_counter);
+
+        pool.execute(move || {
+            let conn_num = counter.fetch_add(1, Ordering::SeqCst);
+            let current = active.fetch_add(1, Ordering::SeqCst) + 1;
+            handle_connection(stream, conn_num, current);
+            active.fetch_sub(1, Ordering::SeqCst);
+        });
     }
 }
 
-fn handle_connection(mut stream: TcpStream){
-    // Creating a BufReader to read from the stream
+fn handle_connection(mut stream: TcpStream, conn_num: usize, active: usize) {
     let buf_reader = BufReader::new(&stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
 
-    let contents = fs::read_to_string(filename).unwrap();
-    let length = contents.len();
+    let mut contents = fs::read_to_string(filename).unwrap();
+    contents = contents.replace("{{CONN_NUM}}", &conn_num.to_string());
+    contents = contents.replace("{{ACTIVE_CONN}}", &active.to_string());
 
+    let length = contents.len();
     let response =
         format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
